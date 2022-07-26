7dd8df253d48d8a142412d5035c3140d7f0455ce
ariel <arirdoyl@gmail.com>
2022-07-26 23:55:52 +0000

    minor change

--- helloworld.sh
+++ helloworld.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script: Ops 201 Class 02 Ops Challenge Solution
-# Author: Ariel Doyle                   
+# Author: Ariel D.                  
 # Date of latest revision: 26JUL2022      
 # Purpose: Print a string to the terminal
 
