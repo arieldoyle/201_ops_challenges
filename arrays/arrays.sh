f38802c0a8c196d863b9498d8ce017e7a83b3324
Ariel Doyle <arirdoyl@gmail.com>
2022-07-28 23:36:49 +0000

    by golly i did it

---
+++ arrays/arrays.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 04 Ops Challenge Solution
+# Author: Ariel D.                  
+# Date of latest revision: 28JUL2022      
+# Purpose: Function that uses an array to push dirctories and text files
+
+# Main
+# Defines array
+DIRS=(dir1 dir2 dir3 dir4)
+
+#loop to create the directory and text document
+for a in ${DIRS[@]}
+do
+   mkdir "$a"
+   touch "$a"/"new.txt"
+done
+
+# End
