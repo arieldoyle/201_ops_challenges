92803aa07e89d0ced934c63490f46fa617edee6c
Ariel Doyle <arirdoyl@gmail.com>
2022-07-29 23:41:04 +0000

    kill loop

---
+++ loopkill.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 05 Ops Challenge Solution
+# Author: Ariel D.                  
+# Date of latest revision: 29JUL2022      
+# Purpose: Script that kills chrome with a loop
+# Chrome PID (47466)
+
+# Main
+
+#loop to kill chrome process
+while true
+do
+    killall 47466
+done
+
+# End
