24b2a5914165fc752c7e7dba86f301704b175d6d
Ariel Doyle <arirdoyl@gmail.com>
2022-07-29 23:47:15 +0000

    minor edits

--- loopkill.sh
+++ loopkill.sh
@@ -4,14 +4,14 @@
 # Author: Ariel D.                  
 # Date of latest revision: 29JUL2022      
 # Purpose: Script that kills chrome with a loop
-# Chrome PID (47466)
+# Chrome PID (5234)
 
 # Main
 
 #loop to kill chrome process
 while true
 do
-    killall 47466
+    killall 5234
 done
 
 # End
