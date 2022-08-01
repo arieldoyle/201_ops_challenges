94accbc054f4fae90d035847715d1d2dab795f79
Ariel Doyle <arirdoyl@gmail.com>
2022-08-01 18:47:40 +0000

    edits

--- loopkill.sh
+++ loopkill.sh
@@ -3,15 +3,29 @@
 # Script: Ops 201 Class 05 Ops Challenge Solution
 # Author: Ariel D.                  
 # Date of latest revision: 29JUL2022      
-# Purpose: Script that kills chrome with a loop
-# Chrome PID (5234)
+# Purpose: Script that kills the script that they just ran
 
 # Main
 
-#loop to kill chrome process
-while true
-do
-    killall 5234
+startup=n
+until [ $startup = "y" ]
+do  
+    echo "Please enter y to start the ping process"
+    read startup
 done
+
+ping -c 10 192.168.0.1 
+
+PINGPID=$!	
+wait $PINGPID		    
+
+ps -aux
+echo "Ping complete"		
+
+echo "Enter the PID for the command bash loopkill.sh"
+read PID
+
+kill -9 $PID
+
 
 # End
