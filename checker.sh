a47b627775356bf608dcb7430b3fd80f874b2045
Ariel Doyle <arirdoyl@gmail.com>
2022-08-02 14:41:11 +0000

    changeeeee

--- checker.sh
+++ checker.sh
@@ -7,16 +7,24 @@
 
 # Main
 
+#Defines arrays to be called on
 DIRECTORY=("dir1" "dir2" "dir3" "dir4")
 FILE=("file1.txt" "file2.txt" "file3.txt" "file4.txt")
 
+#Prints out current list in arrays
+echo "The current list of directories: ${DIRECTORY[@]}"
+echo "The current list of files: ${FILE[@]}"
+
+#Prompts user if they are looking for a directory or a file and stores it
 echo "Please input d to search for a directory or f to search for a file: "
 read input1
 
+#Loop and Conditional to check if the directory or file is there based on the user inputting a d or a f
+#Adds and closes if it is not
 while [[ $input1 = d ]]
 do
     read -p "Please enter a directory  name: " input2
-        if [ -d $DIRECTORY ]
+        if [[ " ${DIRECTORY[*]} " =~ $input2 ]]
             then   
                 echo "$input2 exists. Goodbye"
                 exit
@@ -26,23 +34,20 @@ do
                 echo "Your directory has been created. Goodbye."
                 exit
         fi
-    done 
 done   
 
 while [[ $input1 = f ]]
 do 
-    echo "Please enter a file name: "
-    read input3
-        if [ -f $FILE]
+    read -p "Please enter a file name: " input3
+        if [[ " ${FILE[*]} " =~ $input3 ]]
             then
                 echo "$input3 exists. Goodbye"
                 exit
-            else [ -f $input3]
+            else [[ -f $input3 ]]
                 echo "$input3 does not exist. A new file is being created. Goodbye"
                 touch $input3
                 exit
         fi
-    done
 done
 
         
