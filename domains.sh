d4f10a55a025c93db3a346c433f92a15619bb67b
Ariel Doyle <arirdoyl@gmail.com>
2022-08-11 15:58:20 +0000

    hello

---
+++ domains.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 13 Ops Challenge Solution
+# Author: Ariel D.                  
+# Date of latest revision: 10AUG2022      
+# Purpose: Show domain information
+
+# Main
+
+# Asks user for input of a website
+read -p "Input a website (example: google.com/bing.com/amazon.com/etc): " input
+
+# Defines function domainAnalyzer that runs whois, dig, host, and nslookup on the user input
+function domainAnalyzer {
+    touch domaininfo.txt
+    whois $input > domaininfo.txt
+    dig $input >> domaininfo.txt
+    host $input >> domaininfo.txt
+    nslookup $input >> domaininfo.txt
+    cat domaininfo.txt
+}
+
+domainAnalyzer
+
+# End
